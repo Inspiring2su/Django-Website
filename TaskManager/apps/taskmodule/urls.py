aaec76f9bb45cab684a4a4d4561ea6d9eb0898da
Inspiring2su <zainab21su@gmail.com>
2024-04-28 12:43:49 +0300

    adding CRUD functions from lecture 9(lab 9)

--- TaskManager/apps/taskmodule/urls.py
+++ TaskManager/apps/taskmodule/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('tasks', views.tasks, name='tasks'), 
     path('task/<int:task_id>', views.task, name='task'),  
+    path('addTask', views.addTask, name='addTask'),
+    path('updateTask/<int:taskId>', views.updateTask, name="updateTask"),
     path('filtertasks', views.filter_tasks, name='filter_tasks'),  
 ]
--- TaskManager/apps/taskmodule/views.py
+++ TaskManager/apps/taskmodule/views.py
@@ -9,6 +9,33 @@ def tasks(request):
     tasks = Task.objects.all() 
     return render(request, 'taskmodule/taskList.html', {'tasks': tasks})
 
+def addTask(request):
+    if request.method == 'POST':
+        task_name_val = request.POST.get('task_name')
+        description_val = request.POST.get('task_description')
+        due_date_val = request.POST.get('due_date')
+        priority_val = request.POST.get('priority')
+        task_obj = Task(name=task_name_val, description=description_val, due_date=due_date_val, priority=priority_val)
+        task_obj.save()
+        return redirect('task_detail', taskId=task_obj.id)
+    return render(request, "taskmodule/addTask.html", {})
+
+def updateTask(request, taskId):
+    task_obj = Task.objects.get(id=taskId)
+    if request.method == 'POST':
+        task_name_val = request.POST.get('task_name')
+        description_val = request.POST.get('task_description')
+        due_date_val = request.POST.get('due_date')
+        priority_val = request.POST.get('priority')
+        task_obj.name = task_name_val
+        task_obj.description = description_val
+        task_obj.due_date = due_date_val
+        task_obj.priority = priority_val
+        task_obj.save()
+        return redirect('task_detail', taskId=task_obj.id)
+    return render(request, "taskmodule/updateTask.html", {'obj': task_obj})
+
+
 def filter_tasks(request):
     if request.method == "POST":
         string = request.POST.get('keyword').lower()
